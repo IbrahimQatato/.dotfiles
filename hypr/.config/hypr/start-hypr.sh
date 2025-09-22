5faa70272e25181a9303a8cf61e36bbed26a5ec0
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2025-09-22 15:26:07 +0200

    HYPR_IS_LAPTOP: is now properly sourced and initialized

--- hypr/.config/hypr/start-hypr.sh
+++ hypr/.config/hypr/start-hypr.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+DIR=/home/ibra/.config/hypr/
+FILE=HYPR_IS_LAPTOP.env
 
 if hyprctl monitors | grep -q "eDP-1"; then
-    echo "env = HYPR_IS_LAPTOP,true" > display_env.conf
+    echo "env = HYPR_IS_LAPTOP,true" > "${DIR}${FILE}"
 else
-    echo "env = HYPR_IS_LAPTOP,false" > display_env.conf
+    echo "env = HYPR_IS_LAPTOP,false" > "${DIR}${FILE}"
 
 fi
 
