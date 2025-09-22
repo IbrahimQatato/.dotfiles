c2465d9d50ec68eb609508f899432e31a09651dd
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2025-09-22 14:41:17 +0200

    Inietalizing HYPR_IS_LAPTOP: needs debugging

--- hypr/.config/hypr/start-hypr.sh
+++ hypr/.config/hypr/start-hypr.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if hyprctl monitors | grep -q "eDP-1"; then
-    export HYPR_IS_LAPTOP=true
+    echo "env = HYPR_IS_LAPTOP,true" > display_env.conf
 else
-    export HYPR_IS_LAPTOP=false
+    echo "env = HYPR_IS_LAPTOP,false" > display_env.conf
+
 fi
 
 # exec Hyprland
