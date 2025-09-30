e7cd1949088954661aeb4127f2eb81636f26b03a
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2025-09-30 13:00:48 +0200

    HYPR:start-hypr.sh; fixed bug by escaping $ character

--- hypr/.config/hypr/start-hypr.sh
+++ hypr/.config/hypr/start-hypr.sh
@@ -3,9 +3,9 @@ DIR=/home/ibra/.config/hypr/
 FILE=HYPR_IS_LAPTOP.env
 
 if hyprctl monitors | grep -q "eDP-1"; then
-    echo "$HYPR_IS_LAPTOP=True" > "${DIR}${FILE}"
+    echo "\$HYPR_IS_LAPTOP=True" > "${DIR}${FILE}"
 else
-    echo "$HYPR_IS_LAPTOP=False" > "${DIR}${FILE}"
+    echo "\$HYPR_IS_LAPTOP=False" > "${DIR}${FILE}"
 
 fi
 
