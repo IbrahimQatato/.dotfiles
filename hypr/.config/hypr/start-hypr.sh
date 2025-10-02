54fb069a2ba5947bfc02ca14ff732b236bdbf0c8
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2025-10-02 21:11:45 +0200

    HYPR: variable should be empty to be false

--- hypr/.config/hypr/start-hypr.sh
+++ hypr/.config/hypr/start-hypr.sh
@@ -5,7 +5,7 @@ FILE=HYPR_IS_LAPTOP.env
 if hyprctl monitors | grep -q "eDP-1"; then
     echo "\$HYPR_IS_LAPTOP=True" > "${DIR}${FILE}"
 else
-    echo "\$HYPR_IS_LAPTOP=False" > "${DIR}${FILE}"
+    echo "\$HYPR_IS_LAPTOP=" > "${DIR}${FILE}"
 
 fi
 
