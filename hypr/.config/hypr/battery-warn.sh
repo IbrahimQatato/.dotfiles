fc0c5244151377078de77962f9336cecba13b22e
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2026-03-23 16:26:12 +0100

    HYPR: battery warning now closes when plugged in

--- hypr/.config/hypr/battery-warn.sh
+++ hypr/.config/hypr/battery-warn.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+get_battery() {
+  upower --dump | grep -E percentage | awk '{print $2}' | tr -d '%' | head -n 1
+}
+
+get_charging() {
+  upower -i $(upower -e | grep BAT) | grep "state" | awk '{print $2}'
+}
+
 while true; do
-  battery=$(upower --dump | grep -E percentage | awk '{print $2}' | tr -d '%' | head -n 1)
-  charging=$(upower -i $(upower -e | grep BAT) | grep "state" | awk '{print $2}')
+  battery=$(get_battery)
+  charging=$(get_charging)
 
   if [[ "$battery" -le 25 && "$charging" != "charging" ]] ; then
     # hyprctl notify 0 5000 0 "  Low battery: ${battery}%" 
@@ -9,12 +18,23 @@ while true; do
     # -u critical: Highlights it in red (usually) and keeps it on screen
     # -h int:value: Tells SwayNC to render a progress bar
     ~/.dotfiles/hypr/.config/hypr/playsound.sh ~/.dotfiles/hypr/.config/hypr/toink.mp3 
-    notify-send -u critical \
+    notify-send  -u critical \
         -h string:x-canonical-private-synchronous:battery_low \
         -h int:value:"$battery" \
         "󰂃 Low Battery Warning" \
-        "<b>${battery}%</b>. CHARGE ME!"
-    sleep 240
+        "<b>${battery}%</b>. CHARGE ME!" 
+
+  while [[ "$charging" != "charging" ]] ; do
+    charging=$(get_charging)
+    if [[ "$charging" == "charging" ]]; then
+          notify-send -t 1 \
+          -h string:x-canonical-private-synchronous:battery_low \
+          "" ""
+    fi
+    sleep 1
+  done
+
+    sleep 210
   else
     sleep 120
   fi
