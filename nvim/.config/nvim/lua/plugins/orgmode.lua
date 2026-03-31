16174497f9479fdebf67e638187dafcb9c5053bb
Ibrahim Qatato <37291619+IbrahimQatato@users.noreply.github.com>
2026-03-31 20:01:53 +0200

    nvim: orgmode- added some options commented out

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -17,6 +17,19 @@ return {
           -- org_toggle_checkbox = "<C-Space>",
         },
       },
+      -- org_agenda_use_time_grid = true,
+      -- org_agenda_time_grid = {
+      --   enabled = true,
+      --   -- which conditions must be true to show the grid
+      --   -- options: today, daily, weekly, require-timed
+      --   display = { "weekly" }, -- change to {'daily'} to show grid on ALL days, not just today
+      --   -- list of times to show (24h format integers)
+      --   times = { 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000 },
+      --   time_separator = "......",
+      --   time_label = "",
+      -- },
+      -- org_agenda_span = "week", -- OR set to a number like 3 to see today + 2 days
+      -- org_agenda_start_on_weekday = false, -- Starts the 'week' view on Today
     }
     -- Experimental LSP support
     vim.lsp.enable "org"
